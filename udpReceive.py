01545f9cdb4bdfff95b695ded79057389e65f85b
Gray Salmond <graysalmond@hotmail.com>
2018-04-02 10:31:27 +1200

    Ready to test UDP on seperate machines

--- udpReceive.py
+++ udpReceive.py
@@ -6,6 +6,9 @@ UDP_PORT = 5005
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 
+ipIs = ""
+
 while True:
 	data, addr = sock.recvfrom(1024)
 	print "recieved message:", data
+	ipIs = data.split()[1]
