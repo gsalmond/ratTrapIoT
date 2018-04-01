b218a8561942a5d4dae75fe3e5455fdf2dfcc92e
Gray Salmond <graysalmond@hotmail.com>
2018-04-02 11:06:21 +1200

    Adding autofinding local ip to udpReceive.py

--- udpReceive.py
+++ udpReceive.py
@@ -1,14 +1,15 @@
 import socket
+import subprocess
 
-UDP_IP = "127.0.0.1"
+IP = subprocess.check_output(["hostname", "-I"]).split()[0]
+UDP_IP = str(IP)[2:-1]
 UDP_PORT = 5005
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 
-ipIs = ""
+print("Host IP: " + UDP_IP)
 
 while True:
 	data, addr = sock.recvfrom(1024)
 	print "recieved message:", data
-	ipIs = data.split()[1]
