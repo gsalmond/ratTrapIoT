2459e4fd8f84f4af0335e91e463341b3e3eb4655
Duncan McDonald <duncanmcd@Duncans-MBP-2.station>
2018-04-03 14:34:16 +1200

    added trhreading and display status

--- hub.py
+++ hub.py
@@ -2,6 +2,47 @@
 
 import socket
 import subprocess
+import treading
+# from senseOutput import SenseOutput
+
+#setup socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#get IP address
+# IP = subprocess.check_output(["hostname", "-I"]).split()[0]
+# myIP = str(IP)[2:-1]
+
+myIP = "127.0.0.1"
+
+#configure socket
+sock.bind((myIP, 9050))
+sock.listen(1) #allow only 1 connection
+connection, client_address = sock.accept()
+
+def displayStatus():
+    try:
+        while True:
+            data = connection.recv(128)
+            status = str(data)[2:-1]
+            if status == "caught":
+                print (data)
+                # o.displayRat(1000)
+                connection.sendall(data)# sends back the data probablly change this
+            elif status == "miss":
+                print (data)
+                # o.displayNoRat(1000)
+                connection.sendall(data)# sends back the data probablly change this
+            elif status == "set":
+                print (data)
+                # o.displayTick(1000)
+                connection.sendall(data)# sends back the data probablly change this
+            else:
+                break
+
+    finally:
+        print ("Closing connection")
+        #connection cleanup
+        connection.close()
 
 # sends individual packets out over a local network to potential rat traps containing ip of hub
 def advertiseHubIP():
@@ -12,3 +53,9 @@ def advertiseHubIP():
         UDP_IP = "192.168.1." + str(i)
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.sendto("RatTrapHub: " + thisIP, (UDP_IP, UDP_PORT))
+
+try:
+    statusThread = threading.Thread(target=displayStatus)
+    discoveryThread = threading.Thread(target=advertiseHubIP)
+except:
+    "Something went wrong with threads"
