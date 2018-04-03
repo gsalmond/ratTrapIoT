5fe2d59eb6690e4669a962e4ed4316ff43e1c1ac
Gray Salmond <graysalmond@hotmail.com>
2018-04-03 14:14:34 +1200

    Getting ready to create proper code

--- prototypes/discoverFunc.py
+++ prototypes/discoverFunc.py
@@ -1,9 +1,17 @@
-# Code to be run continously on a individual trap
-
 import socket
 import subprocess
 
-# to be run when a rat trap is not associated with a hub, returns hub ip address as a string
+#to be used in hub.py
+def advertiseHubIP():
+    thisIP = subprocess.check_output(["hostname", "-I"]).split()[0]
+    thisIP = str(thisIP)#[2:-1]
+    UDP_PORT = 5005
+    for i in range(1,255):
+        UDP_IP = "192.168.1." + str(i)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.sendto("RatTrapHub: " + thisIP, (UDP_IP, UDP_PORT))
+
+#to be used in trap.py
 def getHubIP():
     IP = subprocess.check_output(["hostname", "-I"]).split()[0]
     UDP_IP = str(IP)#[2:-1]
--- prototypes/socketConnectionServer.py
+++ prototypes/socketConnectionServer.py
@@ -1,17 +1,19 @@
 #Server Implementation
 import socket
 import subprocess
-from senseOutput import SenseOutput
+# from senseOutput import SenseOutput
 
 #setup output
-o = SenseOutput("Duncan")
+# o = SenseOutput("Duncan")
 
 #setup socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #get IP address
-IP = subprocess.check_output(["hostname", "-I"]).split()[0]
-myIP = str(IP)[2:-1]
+# IP = subprocess.check_output(["hostname", "-I"]).split()[0]
+# myIP = str(IP)[2:-1]
+
+myIP = "127.0.0.1"
 
 #configure socket
 sock.bind((myIP, 9050))
@@ -24,15 +26,15 @@ try:
         status = str(data)[2:-1]
         if status == "caught":
             print (data)
-            o.displayRat(1000)
+            # o.displayRat(1000)
             connection.sendall(data)# sends back the data probablly change this
         elif status == "miss":
             print (data)
-            o.displayNoRat(1000)
+            # o.displayNoRat(1000)
             connection.sendall(data)# sends back the data probablly change this
         elif status == "set":
             print (data)
-            o.displayTick(1000)
+            # o.displayTick(1000)
             connection.sendall(data)# sends back the data probablly change this
         else:
             break
