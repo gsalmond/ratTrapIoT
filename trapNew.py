c22064567580aa7351ddf727ae844b092fddb459
nzdunk1n <31556737+nzdunk1n@users.noreply.github.com>
2018-04-04 14:06:28 +1200

    working trap.py uses a modified TrapClass

---
+++ trapNew.py
@@ -0,0 +1,68 @@
+import socket
+import subprocess
+import RPi.GPIO as GPIO
+import time
+import threading
+from threading import Thread
+from TrapClass import TrapClass
+
+serverIP = None
+
+# to be run when a rat trap is not associated with a hub, returns hub ip address as a string
+def getHubIP():
+    IP = subprocess.check_output(["hostname", "-I"]).split()[0]
+    UDP_IP = str(IP)
+    UDP_PORT = 5005
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((UDP_IP, UDP_PORT))
+
+    while True:
+        print('Waiting to be discovered...')
+        data, addr = sock.recvfrom(1024)
+        if data.split()[0] == "RatTrapHub:":
+            print('found hub')
+            return data.split()[1]
+        
+def missedSwitch(channel):
+    global t
+    if GPIO.input(4):
+        GPIO.output(9, GPIO.HIGH)
+        print ("red light on")
+        t.setMissedFlag(True)
+    else:
+        GPIO.output(9, GPIO.LOW)
+        print ("red light off")
+        t.setMissedFlag(False)
+        
+
+def setSwitch(channel):
+    global t
+    if GPIO.input(13):
+        GPIO.output(10, GPIO.HIGH)
+        print ("green light on")
+        t.setSetFlag(True)
+    else:
+        GPIO.output(10, GPIO.LOW)
+        print ("green light off")
+        t.setSetFlag(False)
+
+hubIP = getHubIP()
+t = TrapClass(123, "Rat", hubIP, 9050)
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  #input for trap start state / missed state
+GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # input for trap set state
+GPIO.setup(9, GPIO.OUT)
+GPIO.setup(10, GPIO.OUT)
+
+GPIO.add_event_detect(4, GPIO.BOTH)
+GPIO.add_event_detect(13, GPIO.BOTH)
+GPIO.add_event_callback(4, missedSwitch)
+GPIO.add_event_callback(13, setSwitch)
+
+try:
+ while True:
+  time.sleep(1)
+finally:
+ GPIO.cleanup()
